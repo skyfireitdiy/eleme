e846e1416dbf3ad3a42590b633d46e7f91e58ce5
skyfire <skyfireitdiy@hotmail.com>
2019-05-13 17:31:37 +0800

    完善依赖包安装脚本

--- install_deps.bat
+++ install_deps.bat
@@ -1,4 +1,4 @@
 @echo off
 
-pip install --user -i https://pypi.tuna.tsinghua.edu.cn/simple flask flask_login flask_wtf werkzeug pony
+pip install --user -i https://pypi.tuna.tsinghua.edu.cn/simple flask flask_login flask_wtf werkzeug pony pymysql json5
 pause
